61b02fc4556de0eed7569c4deecad7e9af615b7a
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-01 17:51:52 -0300

    Add simple login page

--- oshangyanbura/__init__.py
+++ oshangyanbura/__init__.py
@@ -7,6 +7,16 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
