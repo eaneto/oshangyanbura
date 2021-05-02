c02b9b28f0bc66ca535813cf05aa56e4d025b269
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-02 11:46:43 -0300

    Fix postgres url on migration

--- oshangyanbura/migrate.py
+++ oshangyanbura/migrate.py
@@ -5,8 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+def get_database_url():
+    return os.environ['DATABASE_URL'].replace("postgres", "postgresql")
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config["SQLALCHEMY_DATABASE_URI"] = get_database_url()
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
