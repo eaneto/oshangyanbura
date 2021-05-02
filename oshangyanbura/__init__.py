494e692f837ed831801194fcd0f052e4ef0ed58b
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-02 11:43:11 -0300

    Add a small fix for the database url on heroku

--- oshangyanbura/__init__.py
+++ oshangyanbura/__init__.py
@@ -5,10 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+def get_database_url():
+    return os.environ['DATABASE_URL'].replace("postgres", "postgresql")
+
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default-secret-key")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+    app.config["SQLALCHEMY_DATABASE_URI"] = get_database_url()
     db.init_app(app)
 
     from .main import main_bp
