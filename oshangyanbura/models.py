b64a1d175b37fd044eee8cd334861378eacf877f
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-02 21:37:09 -0300

    Add index on email for customer table

--- oshangyanbura/models.py
+++ oshangyanbura/models.py
@@ -7,6 +7,6 @@ from . import db
 class Customer(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, index=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
